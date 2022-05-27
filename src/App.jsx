895e2f5bab7ab8c821de6461be166690050da11b
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-05-27 19:27:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import WithSubnavigation from './components/Navbar';
+import SimpleSidebar from './components/Sidebar';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <WithSubnavigation />
+     
+       {/* <SimpleSidebar /> */}
+
+       {/* <Form />  */}
+    </div>
+  );
+}
+
+export default App;
+
